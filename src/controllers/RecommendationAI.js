1dce78db0d8eab285c419c4fbfa653f26d898143
Geovanny Garcia Downing <85602062+geogardow@users.noreply.github.com>
2024-02-26 03:18:40 -0600

    OpenAI responding
    OpenAI is responding correctly and giving the right information. That information is parsed now

--- src/controllers/RecommendationAI.js
+++ src/controllers/RecommendationAI.js
@@ -15,13 +15,34 @@ const getRecommendationAI = async (req, res) => {
 
   console.log("Query params: ", query);
 
+  var description = "";
+  var param = "";
+
+  if (query.meal) {
+    description = "You are an expert chef and have to give recommendations for meals to go with in spanish.\
+    I need you to give me the answer in JSON style example, Dessert: Just the name of the dessert, Drink: Just the name of the drink.";
+    param = query.meal
+    
+  } else if (query.dessert) {
+    description = "You are an expert chef and have to give recommendations for desserts to go with in spanish.\
+    I need you to give me the answer in JSON style example, Meal: Just the name of the meal, Drink: Just the name of the drink.";
+    param = query.dessert
+  
+  } else if (query.drink) {
+    description = "You are an expert chef and have to give recommendations for drinks to go with in spanish.\
+    I need you to give me the answer in JSON style example, Meal: Just the name of the meal, Dessert: Just the name of the dessert.";
+    param = query.drink
+
+  }
+
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are an expert chef and have to give recommendations to dishes in spanish. No explanations need. You can get a dish, dessert or drink, whatever you get, you recommend the other two" },
-    {"role": "user", "content": "Cantonés"}],
+    messages: [{ role: "system", content: description},
+    {"role": "user", "content": param}],
     model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.content);
+  return res.status(statusCodes.OK).json(JSON.parse(completion.choices[0].message.content));
 };
 
 module.exports = {
