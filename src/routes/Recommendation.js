42bb5e5447ace92dd8ecc9c58eca2a9659c3a062
LuisPeMoraRod <luismorarod98@estudiantec.cr>
2024-02-25 18:30:08 -0600

    feat: add docker-compose.yaml for local development with nodemon running in the container

--- src/routes/Recommendation.js
+++ src/routes/Recommendation.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+//import controllers
 const { getRecommendation } = require("../controllers/CustomRecommendation");
 const {
   getExternalRecommendation,
 } = require("../controllers/ExternalRecommendation");
 const { getRecommendationAI } = require("../controllers/RecommendationAI");
 
+//assign controller for every route
 router.get("/custom", getRecommendation);
 router.get("/external", getExternalRecommendation);
 router.get("/open_ai", getRecommendationAI);
