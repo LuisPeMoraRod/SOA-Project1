2750e1ba7ba5a006a17bac5fbeee0b785374dec5
Geovanny Garcia Downing <85602062+geogardow@users.noreply.github.com>
2024-02-26 02:44:22 -0600

    First aproach
    The openai api is responding correctly, have to filter answers and correctly pass parameters for the GET

--- src/controllers/RecommendationAI.js
+++ src/controllers/RecommendationAI.js
@@ -1,5 +1,8 @@
 const data = require("../models/meals.json");
 const statusCodes = require("../constants/statusCodes");
+const fetch = require('node-fetch');
+const OpenAI = require( "openai");
+const openai = new OpenAI();
 
 /**
  * Handle AI recommendation request
@@ -7,13 +10,18 @@ const statusCodes = require("../constants/statusCodes");
  * @param {*} res
  * @returns
  */
-const getRecommendationAI = (req, res) => {
+const getRecommendationAI = async (req, res) => {
   const query = req.query; // query params
 
   console.log("Query params: ", query);
 
-  //process request
-  return res.status(statusCodes.OK).json(data);
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are an expert chef and have to give recommendations to dishes in spanish. No explanations need. You can get a dish, dessert or drink, whatever you get, you recommend the other two" },
+    {"role": "user", "content": "Cantonés"}],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
 };
 
 module.exports = {
