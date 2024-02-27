109955ccaf538ac8a579c28f9e1b8e6d3cbefd0b
valesskablanco <valesskablanco@estudiantec.cr>
2024-02-27 16:18:59 -0600

    logic changes

--- src/controllers/DefaultRecommendation.js
+++ src/controllers/DefaultRecommendation.js
@@ -12,31 +12,46 @@ const getRecommendation = (req, res) => {
 
   console.log("Query params: ", query);
 
-  let mealIndex = -1;
+  let recommendationMessage = "";
 
-  // Find the index of the requested meal in the meals array
-  for (let i = 0; i < data.meals.length; i++) {
-    if (data.meals[i].name.toLowerCase() === query.meal.toLowerCase()) {
-      mealIndex = i;
-      break;
+  if (query.meal) {
+
+    const mealIndex = data.meals.findIndex(meal => meal.name.toLowerCase() === query.meal.toLowerCase());
+    if (mealIndex === -1) {
+      return res.status(statusCodes.NOT_FOUND).json({ error: "Meal not found" });
+      
     }
+    const dessert = data.desserts[mealIndex];
+    const drink = data.drinks[mealIndex];
+    recommendationMessage = `Para acompañar ${query.meal} le recomendamos ${dessert.name} y ${drink.name}`;
+  }
+  else if (query.dessert) {
+
+    const dessertIndex = data.desserts.findIndex(dessert => dessert.name.toLowerCase() === query.dessert.toLowerCase());
+    if (dessertIndex === -1) {
+      return res.status(statusCodes.NOT_FOUND).json({ error: "Dessert not found" });
+    }
+    const meal = data.meals[dessertIndex];
+    const drink = data.drinks[dessertIndex];
+    recommendationMessage = `Para acompañar ${query.dessert} le recomendamos ${meal.name} y ${drink.name}`;
+    
+  }
+  else if (query.drink) {
+
+    const drinkIndex = data.drinks.findIndex(drink => drink.name.toLowerCase() === query.drink.toLowerCase());
+    if (drinkIndex === -1) {
+      return res.status(statusCodes.NOT_FOUND).json({ error: "Drink not found" });
+    }
+    const meal = data.meals[drinkIndex];
+    const dessert = data.desserts[drinkIndex];
+    recommendationMessage = `Para acompañar ${query.drink} le recomendamos ${meal.name} y ${dessert.name}`;
+
+  }
+  else {
+    return res.status(statusCodes.BAD_REQUEST).json({ error: "Invalid query" });
   }
 
-  if (mealIndex === -1) {
-    return res.status(statusCodes.NOT_FOUND).json({ error: "Meal not found" });
-  }
-
-  const meal = data.meals[mealIndex];
-  const dessert = data.desserts[mealIndex];
-  const drink = data.drinks[mealIndex];
-
-  const recommendation = {
-    meal: meal.name,
-    dessert: dessert.name,
-    drink: drink.name
-  };
-
-  return res.status(statusCodes.OK).json(recommendation);
+  return res.status(statusCodes.OK).json({ recommendation: recommendationMessage });
 };
 
 module.exports = {
