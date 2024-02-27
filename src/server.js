3e43e3e48fa2f6ffe054d12e56853969bab6a471
valesskablanco <valesskablanco@estudiantec.cr>
2024-02-27 16:58:08 -0600

    bug fixes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require("express"); //Import express framework module
 const morgan = require("morgan"); //Import morgan for middleware to log HTTP requests and errors
-const port = process.env.PORT || 80; //Define port: first checks if available in environment variables
+const port = process.env.PORT || 8080; //Define port: first checks if available in environment variables
 
 const app = express(); //Main express app
 
