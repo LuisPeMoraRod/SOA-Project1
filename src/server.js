9c6ef087932d83e79d91edcfd505688de63c37e1
dmejias08 <dmejias08@estudiantec.cr>
2024-02-27 11:25:55 -0600

    add swagger set up

--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 const express = require("express"); //Import express framework module
+const morgan = require("morgan"); //Import morgan for middleware to log HTTP requests and errors
+const port = process.env.PORT || 80; //Define port: first checks if available in environment variables
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const app = express(); //Main express app
-const port = process.env.PORT || 8080; //Define port: first checks if available in environment variables
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(morgan("tiny")); //Log request
 
 const recommendationRoutes = require("./routes/Recommendation");
 app.use("/recommendation", recommendationRoutes); //Define path for recommendation requests
