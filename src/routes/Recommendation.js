8ade261abc7664cc4d7863108290da4201f9f4f3
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2024-02-29 19:22:16 -0600

    error validation, migrate to handler

--- src/routes/Recommendation.js
+++ src/routes/Recommendation.js
@@ -2,36 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 
-/**
- * Handle custom errors
- * @param {*} req
- * @param {*} res
- * @returns
-*/
-const queryValidatorMiddleware = (req, res, next) => {
-  const query = req.query;
-  const queryParams = Object.keys(query);
-  const validParams = ["meal", "drink", "dessert"];
-  console.log("aqui estoy")
-
-  if (queryParams.length > 2 || queryParams.length === 0) {
-    const err = new Error("Invalid number of query parameters. Must be between 1 and 2.");
-    err.status = 400;
-    return next(err);
-  }
-  
-  for (const param of queryParams) {
-    console.log(param)
-    if (!validParams.includes(query[param])) {
-      const err = new Error(`Invalid query parameter value '${query[param]}'. Must be one of: meal, drink, dessert.`);
-      err.status = 400;
-      return next(err);
-    }
-  }
-  
-  // Proceed to controllers
-  next();
-};
+const errorHandler = require("../errors/RecommendationError");
+router.use(errorHandler.queryValidatorMiddleware);
 
 //import controllers
 const { getRecommendation } = require("../controllers/CustomRecommendation");
@@ -42,25 +14,6 @@ const { getRecommendationAI } = require("../controllers/RecommendationAI");
 router.get("/custom", getRecommendation);
 router.get("/external", getExternalRecommendation);
 router.get("/open_ai", getRecommendationAI);
-
-
-const handleEmptyResponse = (req, res, next) => {
-  const responses = [
-    getRecommendation(req, res),
-    getExternalRecommendation(req, res),
-    getRecommendationAI(req, res)
-  ];
-
-  // Check if all responses are empty
-  const allEmpty = responses.every(response => response === null || (Array.isArray(response) && response.length === 0));
-
-  if (allEmpty) {
-    const err = new Error("Could not find a recommendation for that meal");
-    err.status = 404;
-    return next(err);
-  }
-
-  next();
-};
+console.log("antes");
 
 module.exports = router;
