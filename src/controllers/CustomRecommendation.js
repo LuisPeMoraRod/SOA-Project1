8170e094d1e9d40dbf4d1b47cf3c5de9ead18b52
valesskablanco <valesskablanco@estudiantec.cr>
2024-02-29 18:52:17 -0600

    recommendation with one or two parameters

--- src/controllers/CustomRecommendation.js
+++ src/controllers/CustomRecommendation.js
@@ -10,53 +10,73 @@ const statusCodes = require("../constants/statusCodes");
 const getRecommendation = (req, res) => {
   const query = req.query; // query params
 
-  console.log("Query params: ", query);
+  const queryLength = Object.keys(query).length;
+  const firstParameter = Object.keys(query)[0]
+  const secondParameter = Object.keys(query)[1]
+  const firstParameterValue = Object.values(query)[0]
+  const secondParameterValue = Object.values(query)[1]
+
   var response = {}
 
-  let recommendationMessage = "";
-
-  if (query.meal) {
-
-    const mealIndex = data.meals.findIndex(meal => meal.name.toLowerCase() === query.meal.toLowerCase());
-    if (mealIndex === -1) {
-      return res.status(statusCodes.NOT_FOUND).json({ error: "Meal not found" });
-      
-    }
-    response.meal = data.meals[mealIndex];
-    response.dessert = data.desserts[mealIndex];
-    response.drink = data.drinks[mealIndex];
-    response.recommendationMessage = `Para acompañar ${query.meal} le recomendamos ${response.dessert.name} y ${response.drink.name}`;
-  }
-  else if (query.dessert) {
-
-    const dessertIndex = data.desserts.findIndex(dessert => dessert.name.toLowerCase() === query.dessert.toLowerCase());
-    if (dessertIndex === -1) {
-      return res.status(statusCodes.NOT_FOUND).json({ error: "Dessert not found" });
-    }
-    response.meal = data.meals[dessertIndex];
-    response.dessert = data.desserts[dessertIndex];
-    response.drink = data.drinks[dessertIndex];
-    response.recommendationMessage = `Para acompañar ${query.dessert} le recomendamos ${response.meal.name} y ${response.drink.name}`;
+  if (queryLength === 2) {
     
-  }
-  else if (query.drink) {
+    const firstParameterIndex = data[firstParameter].findIndex(item => item.name === firstParameterValue);
+    const secondParameterIndex = data[secondParameter].findIndex(item => item.name === secondParameterValue);
 
-    const drinkIndex = data.drinks.findIndex(drink => drink.name.toLowerCase() === query.drink.toLowerCase());
-    if (drinkIndex === -1) {
-      return res.status(statusCodes.NOT_FOUND).json({ error: "Drink not found" });
+    if (firstParameterIndex === secondParameterIndex) {
+      
+      const missingParameter = getTwoMissingParameter(firstParameter, secondParameter);
+      const missingParameterValue = data[missingParameter][firstParameterIndex].name
+      response = writeResponse(response, firstParameter, secondParameter, missingParameter, firstParameterValue, secondParameterValue, missingParameterValue)
     }
-    response.meal = data.meals[drinkIndex];
-    response.dessert = data.desserts[drinkIndex];
-    response.drink = data.drinks[drinkIndex];
-    response.recommendationMessage = `Para acompañar ${query.drink} le recomendamos ${response.meal.name} y ${response.dessert.name}`;
 
   }
   else {
-    return res.status(statusCodes.BAD_REQUEST).json({ error: "Invalid query" });
+
+    const firstParameterIndex = data[firstParameter].findIndex(item => item.name === firstParameterValue);
+    const missingParameters = getOneMissingParameter(firstParameter);
+    const missingParameterValue1 = data[missingParameters[0]][firstParameterIndex].name
+    const missingParameterValue2 = data[missingParameters[1]][firstParameterIndex].name
+    response = writeResponse(response, firstParameter, missingParameters[0], missingParameters[1], firstParameterValue, missingParameterValue1, missingParameterValue2)
+    
   }
 
   return res.status(statusCodes.OK).json(response);
 };
+
+const writeResponse = (response, key1, key2, key3, value1, value2, value3) => {
+  response[key1] = value1;
+  response[key2] = value2;
+  response[key3] = value3;
+  return response
+}
+
+const getTwoMissingParameter = (foodType1, foodType2) => {
+  const foodTypes = ["meal", "dessert", "drink"];
+  
+  if (!foodTypes.includes(foodType1) || !foodTypes.includes(foodType2)) {
+    return "Invalid food type";
+  }
+
+  if (foodType1 === foodType2) {
+    return "Same food type";
+  }
+
+  const missingFoodType = foodTypes.find(type => type !== foodType1 && type !== foodType2);
+  return missingFoodType;
+};
+
+const getOneMissingParameter = (foodType) => {
+  const foodTypes = ["meal", "dessert", "drink"];
+
+  if (!foodTypes.includes(foodType)) {
+    return "Invalid food type";
+  }
+
+  const missingFoodTypes = foodTypes.filter(type => type !== foodType);
+  return missingFoodTypes;
+};
+
 
 module.exports = {
   getRecommendation,
